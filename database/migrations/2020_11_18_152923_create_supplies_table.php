cd170a99efaeab469cdacbc7b8933db2cd9f30a6
Daniel Neira <danos.neira@gmail.com>
2020-11-18 11:15:27 -0500

    tables and relationships created

---
+++ database/migrations/2020_11_18_152923_create_supplies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('purchased_at');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('provider_id');
+            // foreign keys
+            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->foreign('product_id')->references('id')->on('products');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplies');
+    }
+}
---
+++ database/migrations/2020_11_18_153715_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('sold_at');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('client_id');
+            // foreign keys
+            $table->foreign('client_id')->references('id')->on('clients');
+            $table->foreign('product_id')->references('id')->on('products');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
